6601c8e49bb0dc842dfce8ec00e751275f1d49f4
Akhila Vadithe <vaditheakhila162@gmail.com>
2026-05-09 13:16:36 +0530

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
-require("dotenv").config();
-const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.use("/api/auth", authRoutes);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((error) => {
-    console.log(error);
-});
-
-app.get("/", (req, res) => {
-    res.send("Backend Running Successfully");
-});
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
